b58b2166fe93cf7b61583e8a546fa952708579fc
YeongJaeLee-Security <dudwo8528@gmail.com>
2025-11-12 20:48:13 +0900

    Initial Commit

---
+++ Basic_SwiftUI/Basic_SwiftUIApp.swift
@@ -0,0 +1,32 @@
+//
+//  Basic_SwiftUIApp.swift
+//  Basic_SwiftUI
+//
+//  Created by yeongjaelee on 11/12/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Basic_SwiftUIApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
